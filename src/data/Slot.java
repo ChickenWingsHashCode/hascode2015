427b8c7eff869f140a2807f886a8a426877e1300
LeoPerard <leo.perard@gmail.com>
2015-03-12 20:03:35 +0100

    add the slot architecture

--- src/data/Slot.java
+++ src/data/Slot.java
@@ -1,12 +1,53 @@
 package data;
 
+/**
+ * A slot can be occupied by a server. A slot can be unavailable
+ * 
+ * @author leo
+ *
+ */
 public class Slot
 {
+	/**
+	 * The server on the spot if it exists
+	 */
+	protected Server server;
 
+	/**
+	 * If a server is unavailable
+	 */
+	protected boolean unavailable;
+
+	public Slot(boolean unvailable)
+	{
+		this.setServer(null);
+		this.setUnavailable(unvailable);
+	}
+
+	public Server getServer()
+	{
+		if (this.server == null)
+			throw new IllegalStateException("The slot is empty");
+
+		return this.server;
+	}
+
+	public void setServer(Server server)
+	{
+		this.server = server;
+	}
+
+	protected void setUnavailable(boolean unavailable)
+	{
+		this.unavailable = unavailable;
+	}
+
+	/**
+	 * @return true if the server is unavailable
+	 */
 	public boolean isUnaivalable()
 	{
-		// TODO unavailable of a slot
-		return false;
+		return this.unavailable;
 	}
 
 }
