4b144e9acdccb70669c22b233e0553451a359852
LeoPerard <leo.perard@gmail.com>
2015-03-12 20:29:21 +0100

    add the pool architecture

--- src/data/Pool.java
+++ src/data/Pool.java
@@ -1,6 +1,48 @@
 package data;
 
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * A pool is a set of servers than share ressources.
+ * 
+ * @author leo
+ *
+ */
 public class Pool
 {
+	/**
+	 * The servers of the pool
+	 */
+	protected Set<Server> servers;
 
+	public Pool()
+	{
+		this.servers = new HashSet<Server>();
+	}
+
+	public Set<Server> getServers()
+	{
+		return this.servers;
+	}
+
+	/**
+	 * Add a server to the pool
+	 * 
+	 * @param server
+	 */
+	public void addServer(Server server)
+	{
+		this.servers.add(server);
+	}
+
+	/**
+	 * Remove the server of the pool
+	 * 
+	 * @param server
+	 */
+	public void removeServer(Server server)
+	{
+		this.servers.remove(server);
+	}
 }
